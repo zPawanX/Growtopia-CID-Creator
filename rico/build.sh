b7488bd4f3dfeb850e8e3493407905f96e2a1d51
zPawanX <104470742+zPawanX@users.noreply.github.com>
2022-04-27 07:16:31 +0700

    Update build.sh

--- rico/build.sh
+++ rico/build.sh
@@ -1,3 +1,3 @@
 sudo apt-get install clang
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o id -w #Build source code into executable
-echo "Tool succesfuly builded, you can start creating ID by typing ./cid"
+echo "Tool succesfuly builded, you can start creating ID by typing ./id"
